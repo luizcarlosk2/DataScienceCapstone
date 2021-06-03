51d08f2368a33dc863c30f052b6639f471df2bb5
luizcarlosk2 <luizcarlos.k2@gmail.com>
2021-06-03 14:08:54 -0500

    Rev B

--- server.R
+++ server.R
@@ -8,10 +8,61 @@
 #
 
 library(shiny)
+library(dplyr)
+library(tibble)
+library(tidyverse)
+library(stringi)
+library(stringr)
+library(tm)
+
+
+df_prediction <- readRDS("data/df_prediction.rds")
+data("mpg")
+
+predict_ngram <- function(sentence_input, maxresult = 3){
+    removelinks <- function(x) {gsub("?(f|ht)tp(s?)://(.*)[.][a-z]+", "", x)}
+    sentence_input <- tolower(sentence_input)
+    sentence_input <- str_squish(sentence_input)
+    sentence_input <- removelinks(sentence_input)
+    sentence_input <- removePunctuation(sentence_input)
+    
+    partial_answer <- tibble(value = character(),
+                             n = integer(),
+                             level = double(),
+                             prediction = character())
+    
+    answer <- partial_answer
+    
+    #answer <- multigram %>% filter(sentence_input == value)
+    
+    while(sapply(strsplit(sentence_input, " "), length) > 0){
+        partial_answer <- df_prediction %>% filter(sentence_input == value) #%>% filter(prediction %in% sentence_options)
+        #print(answer)
+        #print(sentence_input)
+        ifelse (sapply(strsplit(sentence_input, " "), length) <= 1, sentence_input <- "", sentence_input <- sub(".+? ", "", sentence_input))
+        #print(head(answer,1))
+        answer <- rbind(answer,partial_answer)
+    }
+    answer %>% head(maxresult) %>% select(prediction) %>% rename("Next Possible Word(s):" = prediction)
+    
+}
+
+#pred <- data.frame(row.names = "A")
+pred <- predict_ngram("", 3)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    
+    reactive({
+    
+    phrase <- input$text
+    
+    #if (phrase != ""){
+        
+    #}
 
-
-
+    })
+    output$answer_table <- renderTable(predict_ngram(input$text, input$n_pred_result)) 
+    # output$answer <- renderText(paste(input$text,"paste"))
+    
 })
--- ui.R
+++ ui.R
@@ -8,26 +8,71 @@
 #
 
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(fluidPage(theme = shinytheme("slate"),
+                  navbarPage("Data Science Capstone",
+                             tabPanel(
+                                 "Text Prediction",
+                                 h3("Instructions"),
+                                 p("Just type the text in the box and the application will try to predict the next word resit. You can configure the number of words results using the slider bellow."),
+                                 sidebarLayout(
+                                     sidebarPanel(
+                                         p("Configuration"),
+                                         sliderInput("n_pred_result",
+                                                     "Number of maximum words in prediction result:",
+                                                     min = 1,
+                                                     max = 10,
+                                                     value = 3)
+                                     ),
+                                     # Show a plot of the generated distribution
+                                     mainPanel(
+                                         textInput("text", label = h3("Enter Text:"), value = ""),
+                                         # textOutput("answer"),
+                                         tableOutput("answer_table"),
+                                         
+                                     )
+                                 )
+                             ),
+                            
+                             tabPanel("About",
+                                      h2("About this application"),
+                                      p("This is a project of the Data Science Specialization in Coursera of Johns Hopkins University."),
+                                      p("The source code is available in ",a(href="https://luizcarlos.shinyapps.io/DataScienceCapstone/","Github"),"."),
+                                      p("The presentation about this aplication is available in ", a(href="https://rpubs.com/franze/dsc_presentation","Rpubs"),"."),
+                                      p("Author: Luiz Carlos Franzé"),
+                                      br(),
+                                      br(),
+                                      
+                             
+                             h2("References"),
+                             
+                             p("Fridolin Wild, Performance Augmentation Lab (PAL), Oxford Brookes University, UK"),
+                             a(href="https://cran.r-project.org/web/views/NaturalLanguageProcessing.html","CRAN"),
+                             br(),
+                             br(),
+                             
+                             
+                             p("Stanford Course"),
+                             a(href="https://web.stanford.edu/~jurafsky/NLPCourseraSlides.html","Stanford Coursera course"),
+                             br(),
+                             br(),
+                             
+                             p("Ingo Feinerer, Kurt Hornik, David Meyer"),
+                             a(href="https://www.jstatsoft.org/article/view/v025i05","Journal  of Statistical Software"),
+                             br(),
+                             br(),
+                             
+                             p("AbdulMajedRaja RS"),
+                             a(href="https://www.r-bloggers.com/2019/08/how-to-create-unigrams-bigrams-and-n-grams-of-app-reviews/","R-Bloggers"),
+                             
+                             )
+                             
+                 )
 
     # Application title
-    titlePanel("Data Science Capstone"),
 
     # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("n_pred_result",
-                        "Number of words in prediction result:",
-                        min = 1,
-                        max = 10,
-                        value = 3)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            textInput("text", label = h3("Text input"), value = "Enter text..."),
-        )
-    )
 ))
